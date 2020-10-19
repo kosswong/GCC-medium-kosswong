319c1ee53d624a7e6d03f00b57b80a6a850e21ea
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:18 -0400

    File Created by GCC-Challenge-2020

---
+++ cplusplus/Question6/Question6.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string encrypt(string words) {
+    // Participant's code will go here
+    return "answer";
+}
+
+
+int main() {
+    string words;
+    getline(cin, words);
+
+    string result = encrypt(words);
+
+    // Do not remove below line
+    cout << result << "\n";
+    // Do not print anything after this line
+
+    return 0;
+}
