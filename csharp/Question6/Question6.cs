f1f09df50e36d63c2b8eaf1aa3da70b88e7b5952
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:12 -0400

    File Created by GCC-Challenge-2020

---
+++ csharp/Question6/Question6.cs
@@ -0,0 +1,38 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Solution
+{
+    class Solution
+    {
+
+        // You may change this function parameters
+        static String Encrypt(String words)
+        {
+            // Participant's code will go here
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            String words = Console.ReadLine();
+
+            String result = Encrypt(words);
+
+            // Please do not remove the below line.
+            Console.WriteLine(result);
+            // Do not print anything after this line
+        }
+    }
+}
