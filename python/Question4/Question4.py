1219d3181bff7c662c05d6095f7d9cce4ab20c30
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:07 -0400

    File Created by GCC-Challenge-2020

---
+++ python/Question4/Question4.py
@@ -0,0 +1,19 @@
+# Participants may update the following function parameters
+def maximumExpectedMoney(noOfTradesAvailable, maximumTradesAllowed,p,x,y):
+    return -1
+
+def main():
+    # This part may require participants to fill in as well.
+    noOfTradesAvailable, maximumTradesAllowed = list(map(int, input().split()))
+    p = list(map(float, input().split()))
+    x = list(map(float, input().split()))
+    y = list(map(float, input().split()))
+
+    # Participants may update the following function parameters
+    answer = maximumExpectedMoney(noOfTradesAvailable, maximumTradesAllowed,p,x,y)
+    # Do not remove below line
+    print(answer)
+    # Do not print anything after this line
+
+if __name__ == '__main__':
+    main()
