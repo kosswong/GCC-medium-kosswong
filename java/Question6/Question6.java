be4c41a4099f005592e115bdf4116f28a55eb5f6
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:15 -0400

    File Created by GCC-Challenge-2020

---
+++ java/Question6/Question6.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+class Solution {
+
+    static String encrypt(String words) {
+        // Participant's code will go here
+        return "";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String words = scanner.nextLine();
+        scanner.close();
+
+        String result = encrypt(words);
+        // Please do not remove the below line.
+        System.out.println(result);
+        // Do nore print anything after this line
+    }
+}
