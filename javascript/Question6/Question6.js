3447dbbde7861256bd00e0a90d88bdb92df4e837
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:22 -0400

    File Created by GCC-Challenge-2020

---
+++ javascript/Question6/Question6.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function encrypt(words){
+    // Participant's code
+    return -1;
+}
+
+function main() {
+    const words = readLine();
+
+    const result = encrypt(words);
+
+    // Please do not remove the below line.
+    console.log(result)
+    // Do not print anything after this line
+}
