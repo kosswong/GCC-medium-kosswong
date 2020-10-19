628ca0d66eb6272db249390cc6071daf90198e9f
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:58:09 -0400

    File Created by GCC-Challenge-2020

---
+++ python/Question6/Question6.py
@@ -0,0 +1,18 @@
+import math
+# You may change this function parameters
+def encrypt(words):
+    # Participants code will be here
+    return " "
+
+
+def main():
+    words = input()
+
+    answer = encrypt(words)
+
+    # Please do not remove the below line.
+    print(answer)
+    # Do not print anything after this line
+
+if __name__ == '__main__':
+    main()
