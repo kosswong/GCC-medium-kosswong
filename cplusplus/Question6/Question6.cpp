d4ccff986cb6d3e2e3c042466f7ab316a12bb0d7
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-24 17:53:19 +0100

    Commited by GCC-Challenge-2020

--- cplusplus/Question6/Question6.cpp
+++ cplusplus/Question6/Question6.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 string encrypt(string words) {
-    // Participant's code will go here
-    return "answer";
+    words.erase(remove(words.begin(), words.end(), ' '), words.end());
+    string newWords = "";
+    int c = words.size();
+    int x = ceil(sqrt(c));
+    for (int j = 0; j < x; j++, newWords+= " ")
+        for (int i = j; i < c; i+=x)
+            newWords += words[i];
+    return newWords;
 }
 
 
 int main() {
     string words;
     getline(cin, words);
-
     string result = encrypt(words);
-
-    // Do not remove below line
     cout << result << "\n";
-    // Do not print anything after this line
-
     return 0;
 }
