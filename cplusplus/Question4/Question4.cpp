7a350461341ba053fd0ffaf5f32c366ed156ac23
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-24 17:11:35 +0100

    Commited by GCC-Challenge-2020

--- cplusplus/Question4/Question4.cpp
+++ cplusplus/Question4/Question4.cpp
@@ -45,12 +45,12 @@ double maximumExpectedMoney(int n, int m, float p[], float x[], float y[] )
     float store[n];
     
     for(int i = 0; i < n; i++){
-        if(isless(p[i],0.00)) p[i] = 0;
-        if(isgreater(p[i],1.00)) p[i] = 1;
-        if(isless(x[i],0.00)) x[i] = 0;
-        if(isless(y[i],0.00)) y[i] = 0;
-        if(isgreater(x[i],100.00)) x[i] = 100;
-        if(isgreater(y[i],100.00)) y[i] = 100;
+        if(isless(p[i],)) p[i] = 0;
+        if(isgreater(p[i],)) p[i] = 1;
+        if(isless(x[i],0)) x[i] = 0;
+        if(isless(y[i],0)) y[i] = 0;
+        if(isgreater(x[i],100)) x[i] = 100;
+        if(isgreater(y[i],100)) y[i] = 100;
         store[i] = p[i]*x[i]-(1-p[i])*y[i];
     }
     
