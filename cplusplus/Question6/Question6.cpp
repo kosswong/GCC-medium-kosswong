7fc6cfd36de99bca91292b9dc2e985bb0133e164
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-30 01:09:14 -0400

    Commited by GCC-Challenge-2020

--- cplusplus/Question6/Question6.cpp
+++ cplusplus/Question6/Question6.cpp
@@ -2,38 +2,13 @@
 
 using namespace std;
 
-void swap(int *r, int *s)
-{
-   int temp = *r;
-   *r = *s;
-   *s = temp;
-   return;
-}
-
 string encrypt(string words) {
     words.erase(remove(words.begin(), words.end(), ' '), words.end());
     string newWords = "";
     int c = words.size();
-    int x = ceil(sqrt(c));
-    int spaceToFill = x*x-c;
-    
-    for (int i = 0; i < spaceToFill; i++){
-        words += ' ';
-    }
-    
-    int perviousLast = 0;
-    for (int i = 1; i < x; i++){
-        for(int j = 1; j <= x-i; j++){
-            swap(words[i+j+x*perviousLast-1], words[i+j+x*perviousLast+j*(x-1)-1]);
-        }
-        perviousLast++;
-    }
-    
-    for (int i = 1; i <= x-spaceToFill; i++){
-        words.insert(i*x+i-1, " "); 
-    }
-    
-    return words;
+    for (int j = 0, x = ceil(sqrt(c)); j < x; j++, newWords+= " ")
+        for (int i = j; i < c; newWords+=words[i], i+=x){}
+    return newWords;
 }
 
 
