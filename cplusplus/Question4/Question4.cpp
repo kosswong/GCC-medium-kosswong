25b87b2de50bb6b47d0a622936bc8310496fea90
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-07 15:20:55 -0500

    Commited by GCC-Challenge-2020

--- cplusplus/Question4/Question4.cpp
+++ cplusplus/Question4/Question4.cpp
@@ -1,77 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void swap(float* a, float* b)  
-{  
-    float t = *a;  
-    *a = *b;  
-    *b = t;  
-}  
-  
-int partition (float arr[], int low, int high)  
-{  
-    float pivot = arr[high];
-    int i = (low - 1);
-  
-    for (int j = low; j <= high - 1; j++)  
-    {  
-        if (isless(arr[j],pivot))  
-        {  
-            i++; 
-            swap(&arr[i], &arr[j]);  
-        }  
-    }
-    swap(&arr[i + 1], &arr[high]);  
-    return (i + 1);  
-}  
-  
-void quickSort(float arr[], int low, int high)  
-{  
-    if (low < high)  
-    {  
-        int pi = partition(arr, low, high);  
-        quickSort(arr, low, pi - 1);  
-        quickSort(arr, pi + 1, high);
-    }  
-}  
-
-double maximumExpectedMoney(int n, int m, float p[], float x[], float y[] )
+double maximumExpectedMoney(int n, int k, double p[], double x[], double y[] )
 {
-    if(n < 2) n = 1;
-    if(n > 100000) n = 100000;
-    if(m < 2) m = 1;
-    if(m > 100000) m = 100000;
-    float outcome = 0;
-    float store[n];
-    
-    for(int i = 0; i < n; i++){
-        if(isless(p[i],0.00)) p[i] = 0;
-        if(isgreater(p[i],1.00)) p[i] = 1;
-        if(isless(x[i],0.00)) x[i] = 0;
-        if(isless(y[i],0.00)) y[i] = 0;
-        if(isgreater(x[i],100.00)) x[i] = 100;
-        if(isgreater(y[i],100.00)) y[i] = 100;
-        store[i] = p[i]*x[i]-(1-p[i])*y[i];
+    double sum = 0;
+    vector<double> test;
+    for(int i = 0; i < n;i++){
+        if((p[i]*x[i]-(1-p[i])*y[i]) > 0){
+            test.push_back(p[i]*x[i]-(1-p[i])*y[i]);
+        }else{
+            test.push_back(0);
+        }
     }
-    quickSort(store, 0, n - 1);
-    for (int i = n-1; i > n-1-m; i--)
-        if(isgreaterequal(outcome + store[i], outcome))
-            outcome += store[i];
-    return outcome;
+    std::priority_queue<std::pair<double, int>> q;
+    for (int i = 0; i < test.size(); ++i) {
+        q.push(std::pair<double, int>(test[i], i));
+    }
+    for (int i = 0; i < k; ++i) {
+        double ki = q.top().first;
+        sum+=ki;
+        q.pop();
+    }
+    return sum;
 }
+
 
 int main(){
     int n, m;
+
     cin >> n >> m;
-    float p[n], x[n], y[n];
+    double p[n], x[n], y[n];
+
     for(int i = 0; i < n; i++)
         cin >> p[i];
     for(int i = 0; i < n; i++)
         cin >> x[i];
     for(int i = 0; i < n; i++)
         cin >> y[i];
-    float result=maximumExpectedMoney(n,m,p,x,y);
+
+    double result=maximumExpectedMoney(n,m,p,x,y);
+
+    // Do not remove below line
     cout << fixed << setprecision(2) << result << endl;
+    // Do not print anything after this line
 
     return 0;
 }
