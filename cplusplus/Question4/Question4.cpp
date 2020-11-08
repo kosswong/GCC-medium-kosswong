ec90edca3c1265abfbf0f5e83cbf709843061ea8
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-08 20:23:53 +0000

    Commited by GCC-Challenge-2020

--- cplusplus/Question4/Question4.cpp
+++ cplusplus/Question4/Question4.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 double maximumExpectedMoney(int n, int k, double p[], double x[], double y[] )
 {
-    vector<double> test;
-    for(int i = 0; i < n;test.push_back(((p[i]*x[i]-(1-p[i])*y[i]) > 0) ? (p[i]*x[i]-(1-p[i])*y[i]) : 0), i++){}
+    vector<double> t;
+    for(int i = 0; i < n;t.push_back(((p[i]*x[i]-(1-p[i])*y[i]) > 0) ? (p[i]*x[i]-(1-p[i])*y[i]) : 0), i++){}
     std::priority_queue<std::pair<double, int>> q;
-    for (int i = 0; i < test.size(); q.push(std::pair<double, int>(test[i], i)), ++i){}
+    for (int i = 0; i < t.size(); q.push(std::pair<double, int>(t[i], i)), ++i){}
     p[0] = 0;
     for (int i = 0; i < k; ++i) {
         double ki = q.top().first;
